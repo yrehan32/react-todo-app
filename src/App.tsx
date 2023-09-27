ffb8b6798a577105db8793a8ae9237369d561003
yrehan32 <spada.genk02@gmail.com>
2023-09-27 22:38:32 +0700

    feat: todo logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,49 @@ import Modal from "./components/Modal";
 function App() {
   const [newTask, setNewTask] = useState("");
 
+  const [todoList, setTodoList] = useState([] as any[]);
+
+  function saveTodo(e: React.FormEvent) {
+    e.preventDefault();
+
+    setTodoList((currentTodos) => {
+      return [
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          task: newTask,
+          completed: false,
+        },
+      ];
+    });
+
+    setNewTask("");
+  }
+
+  function toggleTodo(id: string, completed: boolean) {
+    setTodoList((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed,
+          };
+        }
+
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id: string) {
+    setTodoList((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={saveTodo}>
         <div className="h-screen flex justify-center items-center">
           <div className="card w-screen ml-5 mr-5 md:w-3/4 md:ml-0 md:mr-0 lg:w-3/6 bg-primary text-primary-content">
             <div className="card-body items-center text-center">
@@ -22,6 +62,7 @@ function App() {
                   className="input input-bordered w-full"
                   value={newTask}
                   onChange={(e) => setNewTask(e.target.value)}
+                  required
                 />
               </div>
 
@@ -51,7 +92,47 @@ function App() {
       </form>
 
       <Modal id="todoList" title="TODO List">
-        Hello World!
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Task</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {todoList.map((todo) => {
+                return (
+                  <tr>
+                    <th>
+                      <label>
+                        <input
+                          type="checkbox"
+                          className="checkbox"
+                          checked={todo.completed}
+                          onChange={(e) =>
+                            toggleTodo(todo.id, e.target.checked)
+                          }
+                        />
+                      </label>
+                    </th>
+                    <td>{todo.task}</td>
+                    <th>
+                      <button
+                        className="btn btn-error btn-xs"
+                        onClick={() => deleteTodo(todo.id)}
+                      >
+                        Delete
+                      </button>
+                    </th>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </Modal>
     </>
   );
